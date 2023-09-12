74f4779ff6539ad6065bde2e16915e17a3ead347
90-days-CC-CP-DIU <iframdewan@gmail.com>
2023-09-12 21:57:26 +0600

    12/09/2023

---
+++ 12 sep/Array.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        int ans = 0;
+        int x;
+        for(int i=0; i<n; i++)
+        {
+            cin >> x;
+            int a = sqrt(x);
+            if(a*a != x)
+                ans=1;
+        }
+        if(ans==1)
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;
+    }
+
+
+
+    return 0;
+}
