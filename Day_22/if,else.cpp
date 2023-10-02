f808910d4f53e2e15db01b25bc243bc902257c0a
90-days-CC-CP-DIU <iframdewan@gmail.com>
2023-10-02 22:14:48 +0600

    2/10/2023

---
+++ Day_22/if,else.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string s,a;
+    cin>> s;
+    cin>> a;
+    reverse(s.begin(), s.end());
+    if(s==a)
+    {
+        cout << "YES";
+    }
+    else
+    {
+       cout << "NO";
+    }
+
+    return 0;
+}
